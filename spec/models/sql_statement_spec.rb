07c70ad02061f39b57d4d13b9cdc3f42ba55ac2c
David Huie <david@nationbuilder.com>
2013-07-22 18:58:27 -0700

    Use a float in spec

--- spec/models/sql_statement_spec.rb
+++ spec/models/sql_statement_spec.rb
@@ -5,10 +5,10 @@ describe Swaggie::SQLStatement do
   describe "#duration" do
 
     let(:sql_statement) { Swaggie::SQLStatement.new(:start_time => 2,
-                                                    :end_time => 3) }
+                                                    :end_time => 3.001) }
 
     it "returns the duration of the statement" do
-      sql_statement.duration.should == 1
+      sql_statement.duration.should == 1.001
     end
 
   end

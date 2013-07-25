5149e152f14aa5c48514f0ac9b0dcc80abe5549e
David Huie <david@nationbuilder.com>
2013-07-24 18:34:59 -0700

    Remove stray puts

--- spec/dataset_collection_spec.rb
+++ spec/dataset_collection_spec.rb
@@ -27,10 +27,10 @@ describe Hyphy::DatasetCollection do
     before(:each) { dataset_collection.process_dataset }
 
     it "maps the correct statements to counts" do
-      puts dataset_collection.counts_hash.should ==
+      dataset_collection.counts_hash.should ==
         { "select * from table where id = 4" => 1,
-        "select * from table where id = 7" => 1,
-        "select * from table" => 1 }
+          "select * from table where id = 7" => 1,
+          "select * from table" => 1 }
     end
 
   end
